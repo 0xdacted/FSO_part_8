1d25e3724ff13de17aa838b3ffbcc56478315ea8
Red <crabmarket56@gmail.com>
2023-03-05 23:27:16 +0900

    Add library frontend

---
+++ library-frontend/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/authors">Authors</Link>
+            </li>
+            <li>
+              <Link to="/books">Books</Link>
+            </li>
+            <li>
+              <Link to="/add">Add Book</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/authors" element={<Authors />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/add" element={<NewBook />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ library-frontend/src/queries.js
@@ -0,0 +1,22 @@
+import { gql, useQuery } from '@apollo/client'
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name,
+      born,
+      bookCount,
+    }
+  }
+`
+
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title,
+      author,
+      published,
+    }
+  }
+`
+
