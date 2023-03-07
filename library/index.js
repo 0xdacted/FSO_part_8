013826da4ebdf568ebaa373f872aa46f3bdf9c49
Red <crabmarket56@gmail.com>
2023-03-07 15:22:29 +0900

    Add User model

--- library/index.js
+++ library/index.js
@@ -3,6 +3,7 @@ const { startStandaloneServer } = require('@apollo/server/standalone')
 const { v1: uuid } = require('uuid')
 const Book = require('./models/Book')
 const Author = require('./models/Author')
+const User = require('./models/User')
 
 const mongoose = require('mongoose')
 const { GraphQLError } = require('graphql')
---
+++ library/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3
+  },
+  favoriteGenre: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  }
+})
+
+userSchema.plugin(uniqueValidator)
+
+userSchema.set('toJSON', {
+  transform: (doc, obj) => {
+    obj.id = obj._id.toString()
+    delete obj._id
+    delete obj.__v
+    delete obj.passwordHash
+  }
+})
+
+module.exports = mongoose.model('User', userSchema)
