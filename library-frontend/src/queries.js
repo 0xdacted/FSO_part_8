7b4f07db342c06fe5d8a0aecdb1c4c0ad96fb78e
Red <crabmarket56@gmail.com>
2023-03-08 11:43:14 +0900

    Add ADD_BOOK mutation query

--- library-frontend/src/queries.js
+++ library-frontend/src/queries.js
@@ -24,19 +24,33 @@ export const ALL_BOOKS = gql`
 `
 
 export const EDIT_AUTHOR = gql`
-mutation editAuthor($name: String!, $setBornTo: Int!) {
-  editAuthor(name: $name, setBornTo: $setBornTo) {
-    name
-    born
+  mutation editAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
+      name
+      born
+    }
   }
-}
 `
 
 export const LOGIN = gql`
-mutation login($username: String!, $password: String!) {
-  login(username: $username, password: $password) {
-    value
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      value
+    }
   }
-}
+`
+
+export const ADD_BOOK = gql`
+  mutation addBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+    addBook(title: $title, author: $author, published: $published, genres: $genres) {
+      title
+      author {
+        name
+      }
+      published
+      genres
+      id
+    }
+  }
 `
 
