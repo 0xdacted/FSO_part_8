46127597530256ec8efab69ff90c2652df6e0ca1
Red <crabmarket56@gmail.com>
2023-03-08 13:24:28 +0900

    Add filtering by genre

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,17 +1,43 @@
 import { ALL_BOOKS } from '../queries'
 import { useQuery } from '@apollo/client'
+import { useState } from 'react';
 
 const Books = () => {
   const { loading, error, data } = useQuery(ALL_BOOKS);
+  const [selectedGenres, setSelectedGenres] = useState([])
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {`${error}`}</p>
 
   const books = data.allBooks
+  const genres = books.map(b => b.genres)
+  const uniqueGenres = Array.from(new Set(genres.flat()))
+
+  const filteredBooks = selectedGenres.length > 0 ? books.filter(b => b.genres.some(g => selectedGenres.includes(g))) : books
 
   return (
     <div>
       <h2>books</h2>
+
+      <div>
+        {(uniqueGenres.map(genre => (
+          <label key={genre}>
+            {genre}
+            <input type="checkbox" checked={selectedGenres.includes(genre)}
+            onChange={e => {
+              if (e.target.checked) {
+                setSelectedGenres([...selectedGenres, genre])
+              } else {
+                setSelectedGenres(selectedGenres.filter(g => g !== genre))
+              }
+
+            }}
+          
+            />
+          </label>
+        )))}
+      </div>
+      <button onClick={() => setSelectedGenres([])}>clear filters</button>
 
       <table>
         <tbody>
@@ -20,7 +46,7 @@ const Books = () => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {filteredBooks.map((a) => (
             
             <tr key={a.title}>
               <td>{a.title}</td>
