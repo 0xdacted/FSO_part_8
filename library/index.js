089c97dcecf1a35604ef1f6d8add14c20434e7db
Red <crabmarket56@gmail.com>
2023-03-09 10:51:30 +0900

    Refactor server

--- library/index.js
+++ library/index.js
@@ -27,23 +27,37 @@ mongoose.connect(MONGODB_URI)
     console.log('error connecting to MongoDB:', error.message)
   })
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
+const start = async () => {
+  const app = express()
+  const httpServer = http.createServer(app)
+  
+  const server = new ApolloServer({
+    schema: makeExecutableSchema({ typeDefs, resolvers}),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+  })
 
-startStandaloneServer(server, {
-  listen: { port: 4000 },
-  context: async ({ req, res }) => {
-    const auth = req ? req.headers.authorization : null
-    if (auth && auth.startsWith('Bearer ')) {
-      const decodedToken = jwt.verify(
-        auth.substring(7), JWT_SECRET
-      )     
-      const currentUser = await User.findById(decodedToken.id)
-      return { currentUser }
-    }
-  }
-}).then(({ url }) => {
-  console.log(`Server ready at ${url}`)
-})
+  await server.start()
+
+  app.use(
+    '/',
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        const auth = req ? req.headers.authorization : null
+        if (auth && auth.startsWith('Bearer ')) {
+          const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
+          const currentUser = await User.findById(decodedToken.id)
+          
+          return { currentUser }
+        }
+      }
+    })
+  )
+  const PORT = 4000
+
+  httpServer.listen(PORT, () => 
+    console.log(`Server is now running on http://localhost:${PORT}`))
+}
+
+start()
