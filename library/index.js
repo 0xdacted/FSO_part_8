c33811e55145a7eee19ccacb8cd2a7927477a401
Red <crabmarket56@gmail.com>
2023-03-09 10:56:45 +0900

    Bugfix

--- library/index.js
+++ library/index.js
@@ -46,7 +46,7 @@ const start = async () => {
       context: async ({ req }) => {
         const auth = req ? req.headers.authorization : null
         if (auth && auth.startsWith('Bearer ')) {
-          const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
+          const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET)
           const currentUser = await User.findById(decodedToken.id)
           
           return { currentUser }
